45b3606133dbcf93c78547b4bdae4a44af11d3ec
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2020-07-01 12:47:42 -0400

    solved sherlockandAnagrams hashmap problem

--- sherlockAndAnagrams.js
+++ sherlockAndAnagrams.js
@@ -4,12 +4,12 @@ const getAllSubstrings = (s) => {
     let i, j, result = [];
 
     for (i = 0; i < s.length; i++) {
-        console.log('next')
         for (j = i + 1; j < s.length + 1; j++) {
-            console.log(result.push(s.slice(i, j)));
+            result.push(s.slice(i, j));
 
         }
     }
+    console.log(result);
     return result;
 }
 
@@ -25,10 +25,10 @@ const isAnagram = (str1, str2) => {
             hist[char] = 1
         }
     }
-
+    
     for (let j = 0; j < str2.length; j++) {
         const char = str2[j]
-
+        
         if (hist[char]) {
             hist[char]--
         } else {
@@ -38,15 +38,39 @@ const isAnagram = (str1, str2) => {
     return true
 }
 
+const countAnagrams = (currentIndex, arr) => {
+    const currentElement = arr[currentIndex];
+    const arrRest = arr.slice(currentIndex + 1);
+    let counter = 0;
 
-
-
+    for (let i = 0; i < arrRest.length; i++) {
+        if (currentElement.length === arrRest[i].length && isAnagram(currentElement, arrRest[i])) {
+            counter++;
+        }
+    }
+    return counter;
+}
 
 const sherlockAndAnagrams = (s) => {
+    const duplicatesCount = s.split('').filter((v, i) => s.indexOf(v) !== i);
 
+    if (!duplicatesCount) {
+        return 0;
+    }
 
+    let anagramsCount = 0;
+
+    const arr = getAllSubstrings(s);
+
+    for(let i = 0; i <arr.length; i++){
+        anagramsCount += countAnagrams(i, arr)
+    }
+
+    return anagramsCount;
 
 }
+
+console.log(sherlockAndAnagrams(s));
 
 //1. iterate through the string
 //2. check for annagrams of different length
