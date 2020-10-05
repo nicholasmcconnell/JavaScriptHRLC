9c2b7d0e2104213793ac81716b03dd724d461410
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2020-10-05 11:16:33 -0400

    sudo coded count triplets function

---
+++ countTriplets.js
@@ -0,0 +1,11 @@
+const arr = [1, 2, 2, 4];
+const r = 2;
+const countTriplets = (arr, r) => {
+    //1. find triplets
+    //2. count triplets
+    let triplet;
+    arr.forEach(e => {
+        
+    });
+
+}
