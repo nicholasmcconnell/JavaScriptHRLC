c12920a83f6a8b75f167808318d953af223db69a
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2020-10-11 16:03:03 -0400

    accounts for target of 0 but needs to account for negative numbers

--- twoSum.js
+++ twoSum.js
@@ -1,28 +1,42 @@
-const nums = [2,7,11,15];
-const target = 9;
+const nums = [0, 4, 3, 0];
+const target = 4;
 
 const twoSums = (nums, target) => {
     const remainderMap = {};
     const solution = [];
 
+    for (const [i, e] of nums.entries()) {
+        let remainder = target - e;
+
+        if ((remainder >= 0) && remainderMap[remainder] !== undefined) {
+            solution.push(remainderMap[remainder], i )
+            return solution;
+        } else if (remainder >= 0 && !remainderMap[remainder]) {
+            remainderMap[e] = i;
+        }
+    }
+}
+
+console.log('result', twoSums(nums, target))
+
+//sudo code
     //if remainder >0 &&< 9  check hm heys for remainder 
     //if in hm - return that hm value(index) and current(value)
     // if not push to hashmap - key(value) : value,(index)
 
-    nums.forEach((e, i) => {
-        let remainder = target-e;
 
-        console.log('line14', remainderMap[remainder], remainder, i)
-        
-        if((remainder > 0) && remainderMap[remainder] !== undefined){
-            // console.log('success')
-           return (i, remainderMap[remainder]);
-        } else if (remainder > 0 && !remainderMap[remainder]){
-            console.log('in elseif')
-            remainderMap[e] = i;
-        }
-    });
+//forEach Solution
 
-}
+// nums.forEach((e, i) => {
+    //     let remainder = target-e;
 
-console.log(twoSums(nums, target))
+    // console.log('line14', remainderMap[remainder], remainder, i)
+
+    // if((remainder > 0) && remainderMap[remainder] !== undefined){
+    //     // console.log('success')
+    //    return (i, remainderMap[remainder]);
+    // } else if (remainder > 0 && !remainderMap[remainder]){
+    //     console.log('in elseif')
+    //     remainderMap[e] = i;
+    // }
+    // });
