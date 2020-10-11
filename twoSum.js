48db7ab706f0fae26cf628018a96e618450be8dc
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2020-10-11 16:28:06 -0400

    solution now accounts for negative numbers and zero's in array and zero as a target number.

--- twoSum.js
+++ twoSum.js
@@ -1,6 +1,7 @@
-const nums = [0, 4, 3, 0];
-const target = 4;
+// const nums = [0, 4, 3, 0];
+// const target = 0;
 
+////solution for target lenght of zero and arrays with all positive numbers, but not negative
 const twoSums = (nums, target) => {
     const remainderMap = {};
     const solution = [];
@@ -9,7 +10,7 @@ const twoSums = (nums, target) => {
         let remainder = target - e;
 
         if ((remainder >= 0) && remainderMap[remainder] !== undefined) {
-            solution.push(remainderMap[remainder], i )
+            solution.push(remainderMap[remainder], i)
             return solution;
         } else if (remainder >= 0 && !remainderMap[remainder]) {
             remainderMap[e] = i;
@@ -17,26 +18,54 @@ const twoSums = (nums, target) => {
     }
 }
 
-console.log('result', twoSums(nums, target))
+// console.log('result', twoSums(nums, target))
 
 //sudo code
-    //if remainder >0 &&< 9  check hm heys for remainder 
-    //if in hm - return that hm value(index) and current(value)
-    // if not push to hashmap - key(value) : value,(index)
+//if remainder >0 &&< 9  check hm heys for remainder 
+//if in hm - return that hm value(index) and current(value)
+// if not push to hashmap - key(value) : value,(index)
 
 
 //forEach Solution
 
 // nums.forEach((e, i) => {
-    //     let remainder = target-e;
+//     let remainder = target-e;
 
-    // console.log('line14', remainderMap[remainder], remainder, i)
+// console.log('line14', remainderMap[remainder], remainder, i)
 
-    // if((remainder > 0) && remainderMap[remainder] !== undefined){
-    //     // console.log('success')
-    //    return (i, remainderMap[remainder]);
-    // } else if (remainder > 0 && !remainderMap[remainder]){
-    //     console.log('in elseif')
-    //     remainderMap[e] = i;
-    // }
-    // });
+// if((remainder > 0) && remainderMap[remainder] !== undefined){
+//     // console.log('success')
+//    return (i, remainderMap[remainder]);
+// } else if (remainder > 0 && !remainderMap[remainder]){
+//     console.log('in elseif')
+//     remainderMap[e] = i;
+// }
+// });
+
+
+0
+
+const nums = [0,4,3,0];
+// -3, 4, 3, 90
+const target = 0;
+
+//solution to account for negative numbers in array and target of zero
+const twoSumsNeg = (nums, target) => {
+    const remainderMap = {};
+    const solution = [];
+
+    for (const [i, e] of nums.entries()) {
+        let remainder = target - e;
+        console.log(remainder);
+
+
+        if ((remainder || remainder === 0) && remainderMap[remainder] !== undefined) {
+            solution.push(remainderMap[remainder], i)
+            return solution;
+        } else if ((remainder || remainder === 0) && !remainderMap[remainder]) {
+            remainderMap[e] = i;
+        }
+    }
+}
+
+console.log('result', twoSumsNeg(nums, target))
