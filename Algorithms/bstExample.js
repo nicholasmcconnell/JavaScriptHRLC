9ec5c2c7e75dc7ed3436ef0ddd89e3bbb7f89614
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2020-11-19 12:40:16 -0500

    Node and binary search tree created => BST add find min and find max methods coded

---
+++ Algorithms/bstExample.js
@@ -0,0 +1,57 @@
+
+
+class Node {
+    constructor(data, left = null, right = null) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class BST {
+    constructor() {
+        this.root = null;
+    }
+    add(data) {
+        const node = this.root;
+        if (node === null) {
+            this.root = new Node(data);
+            return;
+        } else {
+            const searchTree = (node) => {
+                if (data < node.data) {
+                    if (node.left === null) {
+                        node.left = new Node(data);
+                        return;
+                    } else if (node.left !== null) {
+                        return searchTree(node.left);
+                    }
+                } else if (data > node.data) {
+                    if (node.right === null) {
+                        node.right = new Node(data);
+                        return;
+                    } else if (node.right !== null) {
+                        return searchTree(node.right)
+                    }
+                } else {
+                    return null;
+                }
+            }
+        }
+        return searchTree(node);
+    }
+    findMin(){
+        let current = this.root;
+        while(current.left !== null){
+            current = current.left;
+        }
+        return current.data;
+    }
+    findMax(){
+        let current = this.root;
+        while(current.right !== null){
+            current = current.right;
+        }
+        return current.data;
+    }
+}
