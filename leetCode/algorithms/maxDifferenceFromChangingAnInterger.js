98513ef42cced7ef7ae756ab07e7b617f7e31efa
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2020-12-07 11:57:47 -0500

    maxdiffromchangineaninteger get number functions working, need to account for edge cases

--- leetCode/algorithms/maxDifferenceFromChangingAnInterger.js
+++ leetCode/algorithms/maxDifferenceFromChangingAnInterger.js
@@ -1,16 +1,56 @@
-let num = 555;
+let num = 9288;
+//9288
 
 const maxDiff = (num) => {
-    //conditionals
-        //if num < 10 return 8 
-    //split num to arry
-    //1st number -> x = highest decimal place -> y = 9 in highest decimal place number position - just make largest dec = 9;
-    //2nd number -> if num greater and 2nd highest decimal != highest decimal spot than x = 2nd highest dec place and y = 0, else x = num, y=1
-        // if second dec != 1st than change to zero, else change it to 1.
-    //1st > 2nd -> 1st - 2nd = dif else 2nd - 1st = dif
-    let newNum = parseInt(num.toString().split(''))
-    return newNum;
+ 
+    if (num < 10) {
+        return 8;
+    }
 
+    let numArr = num.toString().split('');
+
+    const firstNum = (numArr) => {
+        let firstArr = numArr.slice();
+        let y = '9';
+        let x;
+        
+        if(firstArr[0] === '9'){
+            x = firstArr.find(e => e !== '9')
+         } else {
+             x = '9';
+         }
+     
+        for (let i = 0; i < firstArr.length; i++) {
+          if (firstArr[i] === x) {
+                firstArr[i] = y;
+            }
+        }
+        return parseInt(firstArr.join(''));
+    }
+    
+    const secondNum = (numArr) => {
+        const secondArr = numArr.slice();
+        let x, y;
+        
+      for (let i = 0; i < secondArr.length; i++) {
+            if (secondArr[i] !== secondArr[i+1] && secondArr[i] !== '9') {
+                x = secondArr[i];
+                y = '0';
+                break;
+            } else {
+                x = secondArr[i]
+                y ='1'
+                break;
+            }
+        }
+     for(let i = 0; i < secondArr.length; i++){
+         if(secondArr[i] === x){
+             secondArr[i] = y;
+         }
+     }
+        return parseInt(secondArr.join(''));
+    }
+    return Math.abs(firstNum(numArr)-secondNum(numArr))
 }
 
 console.log(maxDiff(num));
