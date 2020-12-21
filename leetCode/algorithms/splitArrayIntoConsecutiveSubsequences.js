efe3eb343fd835658941a955de88c95d6a67b30a
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2020-12-21 11:14:15 -0500

    sudo code alt solution to subsequence array splite challange: new approach uses hashmap to collect subsequences and then check length hash map value's - which are arrays

--- leetCode/algorithms/splitArrayIntoConsecutiveSubsequences.js
+++ leetCode/algorithms/splitArrayIntoConsecutiveSubsequences.js
@@ -7,41 +7,65 @@ const isPossible = (nums) => {
     //if next value consecutive add to array shift or push to array
     //check for array length -> if arr.length < 3 return false
     //if it is -> clear array and start again
-    let forwardArr = [];
-    let backwardArr = [];
 
-    for (let i = 0; i < nums.length; i++) {
-        let lastIndex = nums.length - 1;
-        if (i === 0) {
-            forwardArr.push(nums[i]);
-            backwardArr.push(nums[lastIndex])
-        }
-        //Forward Arry
-        if (nums[i + 1] - 1 === nums[i]) {
-            forwardArr.push(nums[i + 1])
-        }
+    // use a class with a method that checks lenghts of hash arrays as things are built
+            //when adding to hash - loop over hash and find last value that is one less than current value in array
+            //if it doesnt exist, then start a new key value pair in object
 
-        if (nums[i] === nums[i + 1] && forwardArr.length >= 3) {
-            forwardArr = [];
-            forwardArr.push(nums[i + 1]);
-        } else if (nums[i] === nums[i + 1] && forwardArr.length < 3) {
-            return false;
-        }
+        //two class method
+            //first loops over array and calls hashmap checker for each
+        // hashchecker method
+            //making it more optimized
+            //if value
+    let hash = {};
+    let duplicates = [];
+    // while(nums.lengh > 0){
 
-        //Backward Arr
-        if (nums[lastIndex - i] === nums[lastIndex - i - 1] + 1) {
-            backwardArr.push(nums[lastIndex - i - 1]);
-        }
+      
+            for(let j = i; j<nums.length; j++){
+                if(nums[j] === nums[j+1]){
 
-        if (nums[lastIndex - i] === nums[lastIndex - i - 1] && backwardArr.length >= 3) {
-            backwardArr = [];
-            backwardArr.push(nums[lastIndex - i - 1])
-        } else if (nums[lastIndex - i] === nums[lastIndex - i - 1] && backwardArr.length < 3) {
-            return false;
-        }
-    }
+                }
+            }
+       
+    // }
+ 
     return true;
 }
 
 const nums = [1,2,3,3,4,4,5,5];
 console.log(isPossible(nums))
+
+// let forwardArr = [];
+// let backwardArr = [];
+
+// for (let i = 0; i < nums.length; i++) {
+//     let lastIndex = nums.length - 1;
+//     if (i === 0) {
+//         forwardArr.push(nums[i]);
+//         backwardArr.push(nums[lastIndex])
+//     }
+//     //Forward Arry
+//     if (nums[i + 1] - 1 === nums[i]) {
+//         forwardArr.push(nums[i + 1])
+//     }
+
+//     if (nums[i] === nums[i + 1] && forwardArr.length >= 3) {
+//         forwardArr = [];
+//         forwardArr.push(nums[i + 1]);
+//     } else if (nums[i] === nums[i + 1] && forwardArr.length < 3) {
+//         return false;
+//     }
+
+//     //Backward Arr
+//     if (nums[lastIndex - i] === nums[lastIndex - i - 1] + 1) {
+//         backwardArr.push(nums[lastIndex - i - 1]);
+//     }
+
+//     if (nums[lastIndex - i] === nums[lastIndex - i - 1] && backwardArr.length >= 3) {
+//         backwardArr = [];
+//         backwardArr.push(nums[lastIndex - i - 1])
+//     } else if (nums[lastIndex - i] === nums[lastIndex - i - 1] && backwardArr.length < 3) {
+//         return false;
+//     }
+// }
