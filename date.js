6a035a24086dc5b58d306b8af94a51fcb40c4867
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-01-12 12:03:28 -0500

    adding weeksPrior function to formatDate() to enable fetching weeks previos weeks dates

--- date.js
+++ date.js
@@ -1,23 +1,31 @@
 
 
 const formatDate = () => {
-
         let d = new Date();
-    
-
         let datesArr = [];
 
-        // console.log(day.getDate())
         for (let i = 0; i <= 6; i++) {
-            let date = i - d.getDay();
+            let date = i - (d.getDay());
             let day = new Date(d.setDate(d.getDate() + date))
-            // console.log(date, d.getDate())
             datesArr.push(`${d.getMonth() + 1}-${day.getDate()}-${d.getFullYear()}`)
         }
 
-        // return datesArr;
-   console.log(`${d.getMonth() + 1}-${d.getDate()}-${d.getFullYear()}`)
-
+        return datesArr;
 }
 
-console.log(formatDate());
+// console.log(formatDate()[0]);
+
+const weeksPrior = () => {
+    let numOfWeeks = 3;
+    let weekLength = 7;
+    let currSun = formatDate()[0].split('-');
+    let currSunMonth = currSun[0];
+    let currSunDay = currSun[1];
+    let curSunYear = currSun[2];
+
+    console.log(currSunMonth, currSunDay, curSunYear)
+    let d = new Date(2021, 0, 10);
+    console.log(d)
+}
+
+weeksPrior();
