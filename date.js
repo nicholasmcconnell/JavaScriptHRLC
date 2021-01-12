bad3ba5e5321cb46d72e92d85673c75cb8b9a004
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-01-12 12:21:27 -0500

    weeksPrior() works by sending formateDate current date month and year as variables and current date as variable minus multiples of 7 to get arrays of dates from the past

--- date.js
+++ date.js
@@ -1,31 +1,47 @@
 
+/////////ORIGINAL FORMATDATE USED INT UTILFUNCTIONS.JS OF FITNESS TRACKER
+// const formatDate = () => {
+//         let d = new Date();
+//         let datesArr = [];
 
-const formatDate = () => {
-        let d = new Date();
-        let datesArr = [];
+//         for (let i = 0; i <= 6; i++) {
+//             let date = i - (d.getDay());
+//             let day = new Date(d.setDate(d.getDate() + date))
+//             datesArr.push(`${d.getMonth() + 1}-${day.getDate()}-${d.getFullYear()}`)
+//         }
 
-        for (let i = 0; i <= 6; i++) {
-            let date = i - (d.getDay());
-            let day = new Date(d.setDate(d.getDate() + date))
-            datesArr.push(`${d.getMonth() + 1}-${day.getDate()}-${d.getFullYear()}`)
-        }
-
-        return datesArr;
-}
+//         return datesArr;
+// }
 
 // console.log(formatDate()[0]);
+
+
+const formatDate = (currSunYear, currSunMonth, currSunDay) => {
+    let d = new Date(currSunYear, currSunMonth, currSunDay);
+    let datesArr = [];
+
+    for (let i = 0; i <= 6; i++) {
+        let date = i - (d.getDay());
+        let day = new Date(d.setDate(d.getDate() + date))
+        datesArr.push(`${d.getMonth() + 1}-${day.getDate()}-${d.getFullYear()}`)
+    }
+
+    return datesArr;
+}
 
 const weeksPrior = () => {
     let numOfWeeks = 3;
     let weekLength = 7;
+    let d = new Date()
     let currSun = formatDate()[0].split('-');
-    let currSunMonth = currSun[0];
-    let currSunDay = currSun[1];
-    let curSunYear = currSun[2];
+    let currSunMonth =  d.getMonth();
+    let currSunDay = d.getDate();
+    let currSunYear = d.getFullYear();
 
-    console.log(currSunMonth, currSunDay, curSunYear)
-    let d = new Date(2021, 0, 10);
-    console.log(d)
+    console.log(currSunYear, currSunMonth, currSunDay)
+    // let d = new Date(currSunYear, currSunMonth, currSunDay-28);
+    let weekArr = formatDate(currSunYear, currSunMonth, currSunDay -21);
+    console.log(weekArr)
 }
 
 weeksPrior();
