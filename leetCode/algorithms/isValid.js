9353e466afacba8d54ed044d72d1c03b3a6cb413
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-02-26 15:47:49 -0500

    pasted alternate solutions for review

--- leetCode/algorithms/isValid.js
+++ leetCode/algorithms/isValid.js
@@ -39,3 +39,24 @@ const isValid = (s) => {
 }
 
 console.log(isValid(s))
+
+var isValid = function (s) {
+    let mapLeft = {
+        '{': 1,
+        '[': 2,
+        '(': 3
+    },
+        mapRight = {
+            '}': 1,
+            ']': 2,
+            ')': 3
+        };
+    let stack = [];
+    for (let i = 0; i < s.length; ++i) {
+        if (mapLeft[s[i]]) {
+            stack.push(s[i])
+        } else if (!stack.length || mapRight[s[i]] != mapLeft[stack.pop()])
+            return false;
+    }
+    return stack.length ? false : true;
+};
