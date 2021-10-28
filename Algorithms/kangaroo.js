d900843146e01914c5ca9a4a9591e4ff2ecd9fa0
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-10-28 09:43:05 -0600

    optimized the kangaroo algorithm by using v=d/t and modifiying it to d=txv and applying that to our variables

--- Algorithms/kangaroo.js
+++ Algorithms/kangaroo.js
@@ -11,25 +11,31 @@
  */
 
 const x1 = 0;
-const v1 = 2;
-const x2 = 5;
-const v2 = 3;
+const v1 = 3;
+const x2 = 4;
+const v2 = 2;
 
 function kangaroo(x1, v1, x2, v2) {
     // Write your code here
     if (v2 > v1) return 'NO';
-    while (x1 <= x2) {
-        //check if x1==x2
-        //yes? return yes
-        //no? add v1 and v2 to correct kangaroos
-        if (x1 === x2) {
-            return 'YES';
-        } else {
-            x1 = x1 + v1;
-            x2 = x2 + v2
-        }
+
+    if ((x2 - x1) % (v1 - v2) === 0) {
+        return 'YES';
+    } else {
+        return 'NO';
     }
-    return 'NO';
+    // while (x1 <= x2) {
+    //     //check if x1==x2
+    //     //yes? return yes
+    //     //no? add v1 and v2 to correct kangaroos
+    //     if (x1 === x2) {
+    //         return 'YES';
+    //     } else {
+    //         x1 = x1 + v1;
+    //         x2 = x2 + v2
+    //     }
+    // }
+    // return 'NO';
 
 }
 
